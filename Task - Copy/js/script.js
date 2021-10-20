254248be7b509d112a40072afaa88b05c9f0b3a4
iftikharbg <iftikharbg@code.edu.az>
2021-10-20 13:16:28 +0400

    Homework

---
+++ Task - Copy/js/script.js
@@ -0,0 +1,20 @@
+1.//function ShowArea(a,b){
+    //console.log(a*b);
+//}
+
+//ShowArea(5,10);
+
+
+
+
+2.//let a = prompt("Eded daxil edin: ")
+//let b, sum = 0;
+//while(a>0){
+    //b = a % 10;
+    //sum = sum * 10 + b;
+    //a = parseInt(a / 10);
+//}
+//console.log(sum);
+
+
+
